118714e8ededdc456dfaf7cacd667c6f23178f5f
Rodion Artyukhin <artyukhin.rodion@gmail.com>
2020-12-01 12:46:23 +0300

    Add Font enum and CGFloat extension

---
+++ ProgrammaticLayout/Enums/Font.swift
@@ -0,0 +1,17 @@
+//
+//  Font.swift
+//  ProgrammaticLayout
+//
+//  Created by Родион on 01.12.2020.
+//  Copyright © 2020 Rodion Artyukhin. All rights reserved.
+//
+
+import UIKit
+
+import UIKit
+
+enum HelveticaNeue {
+    static func medium(size: CGFloat) -> UIFont {
+        UIFont(name: "HelveticaNeue-Medium", size: size.adaptedFontSize)!
+    }
+}
