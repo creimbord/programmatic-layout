8e9d96ec58086264d3ea9aa3f13082b79c48f773
Rodion Artyukhin <artyukhin.rodion@gmail.com>
2020-12-01 12:43:10 +0300

    Finish player layout

---
+++ ProgrammaticLayout/Extensions/UIColorExtension.swift
@@ -0,0 +1,19 @@
+//
+//  UIColorExtension.swift
+//  ProgrammaticLayout
+//
+//  Created by Родион on 01.12.2020.
+//  Copyright © 2020 Rodion Artyukhin. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static var lightGray: UIColor {
+        UIColor(red: 0.85, green: 0.85, blue: 0.85, alpha: 1)
+    }
+    
+    static var darkGray: UIColor {
+        UIColor(red: 0.43, green: 0.45, blue: 0.47, alpha: 1)
+    }
+}
--- ProgrammaticLayout/View/ViewController.swift
+++ ProgrammaticLayout/View/ViewController.swift
@@ -12,7 +12,11 @@ import SnapKit
 class ViewController: UIViewController {
     
     // MARK: - Views
-    lazy var cover = UIView()
+    lazy var cover     = UIView()
+    lazy var timeScale = UIView()
+    lazy var play      = UIView()
+    lazy var rewind    = UIView()
+    lazy var forward   = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,11 +31,19 @@ class ViewController: UIViewController {
 private extension ViewController {
     func addSubviews() {
         view.addSubview(cover)
+        view.addSubview(timeScale)
+        view.addSubview(play)
+        view.addSubview(rewind)
+        view.addSubview(forward)
     }
     
     func setupViews() {
         setupView()
         setupCoverView()
+        setupTiemScaleView()
+        setupPlayView()
+        setupRewindView()
+        setupForwardView()
     }
     
     func setupView() {
@@ -39,8 +51,27 @@ private extension ViewController {
     }
     
     func setupCoverView() {
-        cover.backgroundColor = UIColor.init(red: 216/255, green: 216/255, blue: 216/255, alpha: 1)
-        cover.layer.cornerRadius = 30
+        cover.backgroundColor = .lightGray
+        cover.layer.cornerRadius = 30.HAdapted
+    }
+    
+    func setupTiemScaleView() {
+        timeScale.backgroundColor = .darkGray
+    }
+    
+    func setupPlayView() {
+        play.backgroundColor = .lightGray
+        play.layer.cornerRadius = 75.HAdapted / 2
+    }
+    
+    func setupRewindView() {
+        rewind.backgroundColor = .lightGray
+        rewind.layer.cornerRadius = 38.HAdapted / 2
+    }
+    
+    func setupForwardView() {
+        forward.backgroundColor = .lightGray
+        forward.layer.cornerRadius = 38.HAdapted / 2
     }
 }
 
@@ -48,13 +79,49 @@ private extension ViewController {
 private extension ViewController {
     func setupConstraints() {
         setupCoverConstraints()
+        setupTimeScaleConstraints()
+        setupPlayConstraints()
+        setupRewindConstraints()
+        setupForwardConstraints()
     }
     
     func setupCoverConstraints() {
         cover.snp.makeConstraints { (make) in
-            make.top.equalTo(view).offset(124)
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(104.VAdapted)
             make.centerX.equalTo(view)
-            make.size.equalTo(CGSize(width: 250, height: 250))
+            make.size.equalTo([250, 250].HResized)
+        }
+    }
+    
+    func setupTimeScaleConstraints() {
+        timeScale.snp.makeConstraints { (make) in
+            make.top.equalTo(cover.snp.bottom).offset(60.VAdapted)
+            make.centerX.equalTo(view)
+            make.size.equalTo([300, 3].HResized)
+        }
+    }
+    
+    func setupPlayConstraints() {
+        play.snp.makeConstraints { (make) in
+            make.top.equalTo(timeScale.snp.bottom).offset(50.VAdapted)
+            make.centerX.equalTo(view)
+            make.size.equalTo([75, 75].HResized)
+        }
+    }
+    
+    func setupRewindConstraints() {
+        rewind.snp.makeConstraints { (make) in
+            make.right.equalTo(play.snp.left).offset(-40.HAdapted)
+            make.centerY.equalTo(play)
+            make.size.equalTo([38, 38].HResized)
+        }
+    }
+    
+    func setupForwardConstraints() {
+        forward.snp.makeConstraints { (make) in
+            make.left.equalTo(play.snp.right).offset(40.HAdapted)
+            make.centerY.equalTo(play)
+            make.size.equalTo([38, 38].HResized)
         }
     }
 }
